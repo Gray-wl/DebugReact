07dec40e92d4ecc5e0dda351756b08b8a4528351
wanglei4 <dayee@306>
2021-06-16 18:58:15 +0800

    render大致解读

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import {React, ReactDOM} from "./CONST";
 import "./index.css";
 import jsx from "./pages/ExamplePage";
 
-ReactDOM.render(jsx, document.getElementById("root"));
+// ReactDOM.render(jsx, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(jsx);
 console.log("React", React.version); //sy-log
