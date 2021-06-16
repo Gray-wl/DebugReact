131a968b37fb37ff5c65d489ca113d40c92b5416
wanglei4 <dayee@306>
2021-06-16 13:47:19 +0800

    debugReact

--- src/CONST.js
+++ src/CONST.js
@@ -1,6 +1,6 @@
-import * as React from "react";
-import {useState, Component} from "react";
-import * as ReactDOM from "react-dom";
+import * as React from "./react/packages/react";
+import {useState, Component} from "./react/packages/react";
+import * as ReactDOM from "./react/packages/react-dom";
 
 // import React from "./kreact/";
 // import ReactDOM, {useState} from "./kreact/react-dom";
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import {React, ReactDOM} from "./CONST";
 import "./index.css";
 import jsx from "./pages/ExamplePage";
 
